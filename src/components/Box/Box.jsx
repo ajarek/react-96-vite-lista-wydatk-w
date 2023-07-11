7902c3ddb0441b2c3fc12bcfaece1a09cac01d92
Ajarek <ajarek@poczta.onet.pl>
2023-07-12 00:54:14 +0200

    added render Component in localStorage

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,31 +1,49 @@
-import {saveStorage, fetchStorage,  saveStorageSingle } from '../../helper/localStorage'
+import {
+  saveStorage,
+  fetchStorage,
+  saveStorageSingle,
+} from '../../helper/localStorage'
 import './Box.css'
-import {Form} from '../Form/Form'
+import { Form } from '../Form/Form'
 import Search from '../Search/Search'
 import DataList from '../DataList/DataList'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 const Box = () => {
-  const [selectedValue, setSelectedValue] = useState('');
-  const [newList,setNewList] = useState(fetchStorage('dataSet'))
-  
-  console.log(selectedValue);
+  const [selectedValue, setSelectedValue] = useState('')
+  const [newList, setNewList] = useState([])
 
-  const handleForm=(data)=>{
-    saveStorage(data,'dataSet');
+  const handleForm = (data) => {
+    // Dodajemy nowe dane do localStorage
+    const updatedData = [...newList, { ...data }]
+    saveStorageSingle(updatedData, 'dataSet')
+    // Aktualizujemy stan 'data' w komponencie, co spowoduje ponowne renderowanie
+    setNewList(updatedData)
   }
 
-const handleDelete=(id)=>{
-  const filterList=newList?.filter((nw)=>nw.id !== id)
-  console.log(filterList);
-  setNewList(newList=>newList=filterList)
-  saveStorageSingle(filterList,'dataSet')
-}
+  useEffect(() => {
+    // Odczytujemy dane z localStorage i aktualizujemy stan 'data'
+    const storedData = fetchStorage('dataSet') || []
+    setNewList(storedData)
+  }, [])
+
+  const handleDelete = (id) => {
+    const filterList = newList?.filter((nw) => nw.id !== id)
+    console.log(filterList)
+    setNewList((newList) => (newList = filterList))
+    saveStorageSingle(filterList, 'dataSet')
+  }
 
   return (
     <div className='box'>
-      <Search value={selectedValue} handleChange={(e)=>setSelectedValue(e.target.value)}/>
-      <Form onSubmit={handleForm}/>
-      <DataList list={newList} deleteItem={handleDelete}/>
+      <Search
+        value={selectedValue}
+        handleChange={(e) => setSelectedValue(e.target.value)}
+      />
+      <Form onSubmit={handleForm} />
+      <DataList
+        list={newList}
+        deleteItem={handleDelete}
+      />
     </div>
   )
 }
